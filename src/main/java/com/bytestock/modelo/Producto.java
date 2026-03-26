1b17c356e74aece7a491a88bb0b66417ccf0e476
geracicherof-rgb <gera.cichero.f@gmail.com>
2026-03-26 17:01:44 -0300

    Create Producto.java

---
+++ src/main/java/com/bytestock/modelo/Producto.java
@@ -0,0 +1,26 @@
+package com.bytestock.modelo;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    // Constructor
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+}
